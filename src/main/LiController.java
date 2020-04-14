f5b66bf9f79010d9500f82388d69bb8cb47ec81b
涂亮 <tuliang@corp.netease.com>
2020-04-14 21:43:37 +0800

    fix

--- src/main/LiController.java
+++ src/main/LiController.java
@@ -1,5 +1,8 @@
 package main;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author: 涂亮 （tuliang@corp.netease.com）
  * @date: 2020/4/10 11:40 PM
@@ -7,5 +10,10 @@ package main;
 public class LiController {
     public static void main(String[] args) {
         System.out.println("li");
+
+        Map<String, String> map = new HashMap<>();
+        for(Map.Entry<String,String> entry:map.entrySet())
     }
+
+
 }
